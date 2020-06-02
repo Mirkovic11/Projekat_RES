c95e1b53accd7ef103e803b76ba8909725979a64
markomitric02@gmail.com <markomitric02@gmail.com>
2020-06-02 18:09:13 +0200

    Ucitavanje podataka iz datoteke

---
+++ Projekat/Writer/Writer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Writer
+{
+    public class Writer
+    {
+        private CodeType code;
+
+        public CodeType Code
+        {
+            get { return code; }
+            set { code = value; }
+        }
+        private int vrijednost;
+
+        public int Vrijednost
+        {
+            get { return vrijednost; }
+            set { vrijednost = value; }
+        }
+
+        public Writer()
+        {
+        }
+
+        public Writer(CodeType code, int vrijednost)
+        {
+            Code = code;
+            Vrijednost = vrijednost;
+        }
+    }
+}
