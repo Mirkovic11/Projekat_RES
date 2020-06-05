e317b8f57c4cd659d0b30dba2e21da777f6b0a7c
zeljana.mirkovic98@gmail.com <zeljana.mirkovic98@gmail.com>
2020-06-06 01:04:29 +0200

    Kreiranje interne Strukture

---
+++ Projekat/ReplicatorReceiver/HistoricalCollection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contracts;
+
+namespace ReplicatorReceiver
+{
+    public class HistoricalCollection
+    {
+
+        public List<Poruka> NizPoruka { get; set; }
+
+        public HistoricalCollection()
+        {
+            List<Poruka> nizPoruka = new List<Poruka>();
+        }
+
+        public HistoricalCollection(List<Poruka> nizPoruka)
+        {
+            NizPoruka = nizPoruka;
+        }
+    }
+}
