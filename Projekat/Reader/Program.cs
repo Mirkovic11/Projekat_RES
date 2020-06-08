1c6292a0e14a13ed00693a5024de21307fd1ca02
zeljana.mirkovic98@gmail.com <zeljana.mirkovic98@gmail.com>
2020-06-08 15:23:43 +0200

    Rasporedjivanje po datasetovima
    Slanje podataka Reader komponenti

---
+++ Projekat/Reader/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using Contracts;
+
+namespace Reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(Reader)))
+            {
+                string address = "net.tcp://localhost:6002/IPosaljiDS1";
+                NetTcpBinding binding = new NetTcpBinding();
+                host.AddServiceEndpoint(typeof(IPosaljiDS1), binding, address);
+
+                host.Open();
+                Reader receiver = new Reader();
+
+                Console.WriteLine("Servis je pokrenut na adresi " + address);
+                Console.ReadKey();
+                host.Close();
+            }
+        }
+    }
+}
